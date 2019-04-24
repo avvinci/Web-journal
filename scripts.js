02a6762efd3fd255973775206220826d94cc92c7
avvinci <vinciabhinav7@gmail.com>
2019-04-24 17:06:18 +0530

    emoji added

--- scripts.js
+++ scripts.js
@@ -5,14 +5,33 @@ let sb = document.getElementById('save-button');
 let nameElem = document.getElementById('name');
 let msgElem = document.getElementById('message');
 let listElem = document.getElementById('mylist');
+let selElem = document.getElementById('select'); 
 
 
 sb.onclick = saveJournal ; 
 // nameElem.onchange = 
+function emotionHandler(){
+    let textval = "" ;  
+    if(selElem.value === 'happy'){
+        textval += "😃     "
+    }
+    else if(selElem.value === 'excited'){
+        textval += "😎     "
+    }
+    else if(selElem.value == 'bored'){
+        textval += "🙄     "
+    }
+    else{
+        textval += "😞     "        
+    }
+    return textval ; 
+}
 
 function createList(val,msgval){
-    let newelem = document.createElement('li'); 
-    var text = document.createTextNode(val+ ': '+ msgval);
+    let newelem = document.createElement('li');
+    let textval = emotionHandler() ; 
+    textval +=  val+ ': '+ msgval ;
+    var text = document.createTextNode(textval);
     newelem.appendChild(text);
     listElem.appendChild(newelem) ; 
 }
